0aef471482d1e102d9a88a7a487a9a845a8615e5
Ivan_Polovnikov <i.a.polovnikov@gmail.com>
2021-09-30 14:07:31 +0700

    init commit

---
+++ src/test/java/com/ttsr/demo/SpringThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttsr.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
